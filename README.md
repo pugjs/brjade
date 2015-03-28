3908530254d0d1bcb9bd387d32f43e73e214534e
Thomas Jensen <thomas@src.agency>
2015-03-28 10:44:24 +0100

    0.0.1

---
+++ README.md
@@ -0,0 +1,27 @@
+# brjade
+
+A [Browserify](https://github.com/substack/node-browserify) transform to
+inline the result of calls to `jade.renderFile` and `jade.render`.
+
+Heavily inspired by [brfs](https://github.com/substack/brfs).
+
+In a script that might or might not be browserified:
+
+```js
+var jade = require('jade');
+
+jade.renderFile(__dirname + '/index.jade');
+```
+
+When bundling:
+
+```js
+var fs = require('fs');
+var browerify = require('browerify');
+var brjade = require('brjade');
+
+browserify('index.js')
+	.transform(brjade)
+	.bundle()
+	.pipe(fs.createWriteStream('bundle.js'));
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var path = require('path');
+var through = require('through2');
+var staticModule = require('static-module');
+var quote = require('quote-stream');
+var resolve = require('resolve');
+var jade = require('jade');
+
+module.exports = transform;
+
+function transform( file, opts ){
+	if (/\.json$/.test(file))
+		return through();
+
+	if (!opts)
+		opts = {};
+
+	var vars = {
+		__filename: file,
+		__dirname: path.dirname(file),
+		require: { resolve: resolver }
+	};
+
+	if (opts.vars)
+		Object.keys(opts.vars).forEach(function( key ){
+			vars[key] = opts.vars[key];
+		});
+
+	var sm = staticModule({
+		jade: {
+			renderFile: renderFile,
+			render: render,
+		},
+	}, {
+		vars: vars,
+		varModules: { path: path },
+	});
+
+	function resolver(p){
+		return resolve.sync(p, { basedir: path.dirname(file) });
+	}
+
+	return sm;
+
+	function renderFile( file, opts ){
+		var markup = quote();
+
+		markup.end(jade.renderFile(file, opts));
+
+		sm.emit('file', file);
+
+		return markup;
+	}
+
+	function render( source, opts ){
+		var markup = quote();
+
+		markup.end(jade.render(source, opts));
+
+		return markup;
+	}
+};
